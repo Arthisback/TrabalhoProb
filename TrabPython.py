be4bea5b7590bb868451990ce212758e8a9bd56a
Arthur Bertoglio <arthur.bertoglio@gmail.com>
2024-04-16 20:13:54 -0300

    Corrigindo algumas coisas

--- TrabPython.py
+++ TrabPython.py
@@ -10,29 +10,31 @@ Digite 0 para sair\n""")
             break
 
         if prob == '1':
-            n = int(input("Digite a quantidade de casos totais: "))
-            x = int(input("Digite a quantidade de casos favoráveis: "))
-            p = float(input("Digite a porcentagem de casos favoráveis: "))
+            n = int(input("Digite a quantidade de casos totais(n) : "))
+            x = int(input("Digite a quantidade de casos favoráveis(x): "))
+            p = float(input("Digite a porcentagem de casos favoráveis(p): "))
             p2 = p / 100
             q = 1 - p2
             qexp = n - x
             combination = math.factorial(n) // (math.factorial(n - x) * math.factorial(x))
-            result = combination * pow(p2, x) * pow(q, qexp)
-            print("A probabilidade é de: {:.4f}".format(result))
+            result = (combination * pow(p2, x) * pow(q, qexp))*100
+            print("A probabilidade é de: {:.2f}".format(result))
+            print("\n")
 
         elif prob == '2':
-            n = int(input("Digite a quantidade de casos totais: "))
-            x_min = int(input("Digite o limite inferior de casos favoráveis: "))
-            x_max = int(input("Digite o limite superior de casos favoráveis: "))
-            p = float(input("Digite a porcentagem de casos favoráveis: "))
+            n = int(input("Digite a quantidade de casos totais(n): "))
+            x_min = int(input("Digite o limite inferior de casos favoráveis(X deve ser no minimo igual ou maior este numero): "))
+            x_max = int(input("Digite o limite superior de casos favoráveis(X deve ser no maximo igual ou menor a este numero): "))
+            p = float(input("Digite a porcentagem de casos favoráveis(p): "))
             p2 = p / 100
             q = 1 - p2
             cumulative_prob = 0
             for i in range(x_min, x_max + 1):
                 combination = math.factorial(n) // (math.factorial(n - i) * math.factorial(i))
-                result = combination * pow(p2, i) * pow(q, n - i)
+                result = (combination * pow(p2, i) * pow(q, n - i))*100
                 cumulative_prob += result
-                print("A probabilidade acumulada para", i, "casos favoráveis é de: {:.4f}".format(cumulative_prob))
+            print("A probabilidade acumulada para", i, "casos favoráveis é de: {:.2f}".format(cumulative_prob))
+            print("\n")
         else:
             print("Opção inválida.")
 
